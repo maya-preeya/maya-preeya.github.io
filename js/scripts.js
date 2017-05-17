1fa17cffb702ad9f41693060f6e84a34b9203f62
Andy Kamath <andy@andykamath.com>
2017-05-16 20:00:35 -0400

    swag

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-Parse.initialize("4xorZh7zv0YjtVvF7bbonr05DAFS2OeXhRsHR8LK", "g7PD0mIz3waStfJ9evWnzY0UnX20ZaSnxDcO1gYd");
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBbZdVMDn2G-_JGc6z_y2adfmuN7mhzG50",
+    authDomain: "maya-22bcb.firebaseapp.com",
+    databaseURL: "https://maya-22bcb.firebaseio.com",
+    projectId: "maya-22bcb",
+    storageBucket: "maya-22bcb.appspot.com",
+    messagingSenderId: "164562589107"
+  };
+  firebase.initializeApp(config);
